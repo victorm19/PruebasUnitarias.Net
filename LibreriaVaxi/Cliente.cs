063ce8c6cc57c9087c6d1f5858ab9f3d4895dc6b
Victor Martinez <sofka-desarrollo@baplicada.cl>
2022-08-26 11:15:20 -0500

    Fin Seccion 4

--- LibreriaVaxi/Cliente.cs
+++ LibreriaVaxi/Cliente.cs
@@ -4,13 +4,40 @@ public class Cliente
 {
     public string ClienteNombre { get; set; }
     public int Descuento = 10;
+    public int OrderTotal { get; set; }
 
     public string CrearNombreCompleto(string nombre, string apellido)
     {
+        if (string.IsNullOrWhiteSpace(nombre))
+            throw new ArgumentException("El nombre esta en blanco");
+
         Descuento = 30;
         ClienteNombre = $"{nombre} {apellido}";
 
         return ClienteNombre;
     }
+
+    public TipoCliente GetClienteDetalle()
+    {
+        if (OrderTotal < 500)
+            return new ClienteBasico();
+        else
+            return new ClientePremium();
+    }
+}
+
+public class TipoCliente
+{
+
+}
+
+public class ClienteBasico : TipoCliente
+{
+
+}
+
+public class ClientePremium : TipoCliente
+{
+
 }
 
--- LibreriaVaxiNUnitTests/ClienteNUnitTest.cs
+++ LibreriaVaxiNUnitTests/ClienteNUnitTest.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System;
 
 namespace LibreriaVaxi;
 
@@ -19,12 +20,15 @@ public class ClienteNUnitTest
         string resultadoExpected = "Victor Martinez";
         _cliente.CrearNombreCompleto("Victor", "Martinez");
 
-        Assert.That(_cliente.ClienteNombre, Is.EqualTo(resultadoExpected));
-        Assert.AreEqual(_cliente.ClienteNombre, resultadoExpected);
-        Assert.That(_cliente.ClienteNombre, Does.Contain("Martinez"));
-        Assert.That(_cliente.ClienteNombre, Does.Contain("martinez").IgnoreCase); //Ignora mayusculas y minusculas
-        Assert.That(_cliente.ClienteNombre, Does.StartWith("Victor"));
-        Assert.That(_cliente.ClienteNombre, Does.EndWith("Martinez"));
+        Assert.Multiple(() =>
+        {
+            Assert.That(_cliente.ClienteNombre, Is.EqualTo(resultadoExpected));
+            Assert.AreEqual(_cliente.ClienteNombre, resultadoExpected);
+            Assert.That(_cliente.ClienteNombre, Does.Contain("Martinez"));
+            Assert.That(_cliente.ClienteNombre, Does.Contain("martinez").IgnoreCase); //Ignora mayusculas y minusculas
+            Assert.That(_cliente.ClienteNombre, Does.StartWith("Victor"));
+            Assert.That(_cliente.ClienteNombre, Does.EndWith("Martinez"));
+        });
     }
 
     [Test]
@@ -38,6 +42,49 @@ public class ClienteNUnitTest
     {
         int descuento = _cliente.Descuento;
         Assert.That(descuento, Is.InRange(5, 24));
+    }
+
+    [Test]
+    public void CrearNombreCompleto_InputNombre_RetornaNotNull()
+    {
+        _cliente.CrearNombreCompleto("Vaxi", "");
+
+        Assert.IsNotNull(_cliente.ClienteNombre);
+        Assert.IsFalse(string.IsNullOrEmpty(_cliente.ClienteNombre));
+    }
+
+    [Test]
+    public void ClienteNombre_InputNombreEnBlanco_ThrowException()
+    {
+        var exceptionDetalle = Assert.Throws<ArgumentException>(() => _cliente.CrearNombreCompleto("", "Martinez"));
+
+        Assert.AreEqual("El nombre esta en blanco", exceptionDetalle.Message);
+        Assert.That(() => 
+            _cliente.CrearNombreCompleto("", "Martinez"), Throws.ArgumentException.With.Message.EqualTo("El nombre esta en blanco"));
+
+        Assert.Throws<ArgumentException>(() => _cliente.CrearNombreCompleto("", "Martinez"));
+        Assert.That(() =>
+            _cliente.CrearNombreCompleto("", "Martinez"), Throws.ArgumentException);
+    }
+
+    [Test]
+    public void GetClienteDetalle_CrearClienteConMenos500TotalOrder_ReturnsClienteBasico()
+    {
+        _cliente.OrderTotal = 150;
+
+        var resultado = _cliente.GetClienteDetalle();
+
+        Assert.That(resultado, Is.TypeOf<ClienteBasico>());
+    }
+
+    [Test]
+    public void GetClienteDetalle_CrearClienteConMas500TotalOrder_ReturnsClientePremium()
+    {
+        _cliente.OrderTotal = 700;
+
+        var resultado = _cliente.GetClienteDetalle();
+
+        Assert.That(resultado, Is.TypeOf<ClientePremium>());
     }
 }
 
